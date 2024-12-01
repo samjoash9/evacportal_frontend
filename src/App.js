357e0dd77bb381e41e27b155e7148a25de40c501
samjoash9 <113993739+samjoash9@users.noreply.github.com>
2024-12-01 23:01:38 +0800

    frontend added build

--- src/App.js
+++ src/App.js
@@ -1,31 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
-  const [backendData, setBackendData] = useState(null); // Initialize as null to handle the loading state
+  const [data, setData] = useState(""); 
 
-  useEffect(() => {
-    fetch("/json")
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data); // Set the fetched data
-      });
-  }, []);
+  const getData=async()=>{
+    const response = await Axios.get("https://evacportal-backend.onrender.com/json");
+    setData(response.data);
+  }
+
+    useEffect(()=>{
+      getData()
+    }, []);
 
   return (
     <div>
       <h1>Backend Data</h1>
       
-      {/* Check if data is still loading */}
-      {backendData === null ? (
+      {data === null ? (
         <p>Loading...</p>
       ) : (
         <div>
-          <p><strong>Message:</strong> {backendData.message}</p>
-          <p><strong>Status:</strong> {backendData.status}</p>
-
           <h2>Data</h2>
-          <p><strong>ID:</strong> {backendData.data.id}</p>
-          <p><strong>Name:</strong> {backendData.data.name}</p>
+          <p><strong>Name:</strong> {data.message}</p>
         </div>
       )}
     </div>
